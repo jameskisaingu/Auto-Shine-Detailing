959043da7b5d469cea1544b000ef977c828269ec
jameskisaingu <jameskisaingu04@gmail.com>
2025-12-24 01:26:59 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let slides = document.querySelectorAll('.slide');
+let current = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+  });
+  slides[index].classList.add('active');
+}
+
+function nextSlide() {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}
+showSlide(current);
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
